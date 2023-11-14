af14d2f663312f2381678281e246fa023c69a914
Anani Samuel <ananisamuelhope@gmail.com>
2023-11-14 16:28:42 +0300

    navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,18 +12,18 @@ const Navbar = () => {
     <div className=' bg-[#173e26]'>
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='w-[30%] text-3xl font-bold'>ACE IDH</h1>
-        <ul className='flex hidden'>
+        <ul className='hidden md:flex'>
             <li className='p-4'>Home</li>
             <li className='p-4'>Regulatory & Strategy Repository</li>
             <li className='p-4'>Investment Life cycle</li>
             <li className='p-4'>Data Repository</li>
             <li className='p-4'>ACE Services</li>
         </ul>
-        <div onClick={handleNav}>
+        <div onClick={handleNav} className='block md:hidden'>
             {!nav ? <AiOutlineClose size={30}/> : <AiOutlineMenu size={30}/> }
         </div>
-        <div className='fixed left-0 top-0 w-[60%] bg-[#173e26]  h-full'>
-            <h1 className='w-[30%] text-3xl font-bold m-4'>ACE IDH</h1>
+        <div className={!nav ? 'fixed left-0 top-20 w-[100%] bg-[#173e26]  h-full ease-in-out duration-500': 'fixed top-[-100%]'}>
+            {/* <h1 className='w-[30%] text-3xl font-bold m-4'>ACE IDH</h1> */}
             <ul className=' p-4'>
                 <li className='p-4 border-b border-gray-600'>Home</li>
                 <li className='p-4 border-b border-gray-600'>Regulatory & Strategy Repository</li>
